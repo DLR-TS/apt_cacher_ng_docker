4bdc7e8a90c677a01f9d6e7d795e5812278b4ff3
alhadrad@gmail.com <akoerner>
2022-11-10 15:55:05 +0100

    WIP: git branch as docker tag

--- apt_cacher_ng_docker.mk
+++ apt_cacher_ng_docker.mk
@@ -1,23 +1,27 @@
 
-ROOT_DIR=$(shell dirname "$(realpath $(firstword $(MAKEFILE_LIST)))")
-#apt_cacher_ng_docker_MAKEFILE_PATH:= $(shell dirname "$(abspath "$(lastword $(MAKEFILE_LIST))")")
-apt_cacher_ng_docker_MAKEFILE_PATH:=$(shell realpath "$(shell dirname "$(lastword $(MAKEFILE_LIST))")")
+ifndef APT_CACHER_NG_DOCKER_MAKEFILE_PATH 
+
+MAKEFLAGS += --no-print-directory
+
+APT_CACHER_NG_DOCKER_MAKEFILE_PATH:=$(shell realpath "$(shell dirname "$(lastword $(MAKEFILE_LIST))")")
 
 
 MAKEFLAGS += --no-print-directory
 
 .PHONY: start_apt_cacher_ng
 start_apt_cacher_ng: ## starts apt-cacher-ng service
-	cd "${apt_cacher_ng_docker_MAKEFILE_PATH}" && make up
+	cd "${APT_CACHER_NG_DOCKER_MAKEFILE_PATH}" && make up
 
 .PHONY: stop_apt_cacher_ng
 stop_apt_cacher_ng: ## starts apt-cacher-ng service
-	cd "${apt_cacher_ng_docker_MAKEFILE_PATH}" && make down
+	cd "${APT_CACHER_NG_DOCKER_MAKEFILE_PATH}" && make down
 
 .PHONY: check_apt_cacher_service
 check_apt_cacher_service: ## Returns the status of the apt-cacher ng service
-	@cd ${apt_cacher_ng_docker_MAKEFILE_PATH} && bash check_apt_cacher_service_status.sh
+	@cd ${APT_CACHER_NG_DOCKER_MAKEFILE_PATH} && bash check_apt_cacher_service_status.sh
 
 .PHONY: get_cache_statistics
 get_cache_statistics: ## Returns the caching statistics of  apt cacher ng
-	@cd ${apt_cacher_ng_docker_MAKEFILE_PATH} && bash get_cache_statistics.sh
+	@cd ${APT_CACHER_NG_DOCKER_MAKEFILE_PATH} && bash get_cache_statistics.sh
+
+endif
