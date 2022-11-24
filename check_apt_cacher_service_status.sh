7cdde658e46aa9afb47c0462466d55704f82fa10
alhadrad@gmail.com <akoerner>
2022-11-24 14:13:26 +0100

    added retry flags to curl

--- check_apt_cacher_service_status.sh
+++ check_apt_cacher_service_status.sh
@@ -8,6 +8,9 @@ else
 fi
 
 status=$(curl --silent \
+              --retry 5 \
+              --retry-all-errors \
+              --retry-max-time 2 \
               --output /dev/null \
               --head \
               --location \
