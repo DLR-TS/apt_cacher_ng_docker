82d0c0573968de2891a594598ad2c2dc073632e4
akoerner1 <andrew.koerner@dlr.de>
2023-01-25 13:00:57 +0100

    update purg script

--- check_apt_cacher_service_status.sh
+++ check_apt_cacher_service_status.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env sh
 
-
-if [ -f /.dockerenv ]; then
-    url='http://host.docker.internal:3142/'
-else
-    url='http://127.0.0.1:3142/'
-fi
-
-sleep 1s
+url='http://127.0.0.1:3142/'
 status=$(curl --silent \
-              --retry 5 \
+              --retry 2 \
+              --retry-delay 1 \
               --retry-max-time 2 \
               --output /dev/null \
               --head \
@@ -20,7 +14,23 @@ status=$(curl --silent \
         )
 
 case $status in
-  (40*) echo "Apt-Cacher service is: UP";;
-  (*) echo "Apt-Cacher service is: DOWN start it with 'make start_apt_cacher_ng'" && exit 1;;
+  (40*) echo "Apt-Cacher service is: UP" && exit 0;;
 esac
 
+url='http://host.docker.internal:3142/'
+status=$(curl --silent \
+              --retry 2 \
+              --retry-delay 1 \
+              --retry-max-time 2 \
+              --output /dev/null \
+              --head \
+              --location \
+              --connect-timeout 5 \
+              --fail \
+              --write-out %{http_code} ${url}
+        )
+
+case $status in
+  (40*) echo "Apt-Cacher service is: UP" && exit 0;;
+  (*) echo "Apt-Cacher service is: DOWN start it with 'make start_apt_cacher_ng'" && exit 1;;
+esac
--- purge_apt_cacher_damaged_packages.sh
+++ purge_apt_cacher_damaged_packages.sh
@@ -1,31 +1,38 @@
 #!/usr/bin/env sh
 
+printf "Auto-purging Apt cacher ng damaged packages...\n"
+request="acng-report.html?abortOnErrors=aOe&byPath=bP&byChecksum=bS&truncNow=tN&incomAsDamaged=iad&purgeNow=pN&doExpire=Start+Scan+and%2For+Expiration&calcSize=cs&asNeeded=an#bottom"
 
-if [ -f /.dockerenv ]; then
-    url='http://host.docker.internal:3142/'
-else
-    url='http://127.0.0.1:3142/'
-fi
+url='http://127.0.0.1:3142/'
+(curl --silent \
+     --retry 2 \
+     --retry-delay 1 \
+     --retry-max-time 2 \
+     --output /dev/null \
+     --head \
+     --location \
+     --connect-timeout 5 \
+     --fail \
+     ${url}${request} & )
 
-## curl 'http://127.0.0.1:3142/acng-report.html?abortOnErrors=aOe&incomAsDamaged=iad&justRemoveDamaged=Delete+damaged&calcSize=cs&asNeeded=an#bottom' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'Connection: keep-alive' -H 'Referer: http://127.0.0.1:3142/acng-report.html' -H 'Upgrade-Insecure-Requests: 1' -H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-User: ?1'
+url='http://host.docker.internal:3142/'
+(curl --silent \
+     --retry 2 \
+     --retry-delay 1 \
+     --retry-max-time 2 \
+     --output /dev/null \
+     --head \
+     --location \
+     --connect-timeout 5 \
+     --fail \
+     ${url}${request} & )
 
-## curl 'http://127.0.0.1:3142/acng-report.html?abortOnErrors=aOe&byPath=bP&byChecksum=bS&incomAsDamaged=iad&justRemoveDamaged=Delete+damaged&calcSize=cs&asNeeded=an#bottom' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'Connection: keep-alive' -H 'Referer: http://127.0.0.1:3142/acng-report.html' -H 'Upgrade-Insecure-Requests: 1' -H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-User: ?1'
 
-status=$(curl --silent \
-              -H "Referer: ${url}/acng-report.html" \
-              --retry 5 \
-              --retry-max-time 2 \
-              --output /dev/null \
-              --head \
-              --location \
-              --connect-timeout 5 \
-              --fail \
-              --write-out %{http_code} ${url} \
-              "${url}/acng-report.html?abortOnErrors=aOe&byPath=bP&byChecksum=bS&incomAsDamaged=iad&justRemoveDamaged=Delete+damaged&calcSize=cs&asNeeded=an#bottom"
-        )
 
-case $status in
-  (40*) echo "Purged apt cacher ng damaged packages.";;
-  (*) echo "Apt-Cacher service is DOWN, cannot purge damaged packages." && exit 1;;
-esac
+
+
+
+
+
+
 
